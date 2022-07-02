bf35a047123e01c58f677a3c9214304d73ae3333
thedronit <thedronit@gmail.com>
2022-07-02 23:24:41 +1000

    0207222324

--- web/admin.py
+++ web/admin.py
@@ -1,19 +1,26 @@
+from .models import *
 from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.utils.translation import ugettext_lazy as _
-from .models import *
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
+
+class ProductAdminForm(forms.ModelForm):
+    About = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 class ProductAdmin(admin.ModelAdmin):
-	filter_horizonal = ('Images',)
+    form = ProductAdminForm
 
 admin.site.register(ProductImage)
 admin.site.register(Product, ProductAdmin)
-
-
 admin.site.register(Category)
 admin.site.register(Manufacturer)
 admin.site.register(Tag)
 admin.site.register(Size)
-
 admin.site.register(Basket)
+
+
